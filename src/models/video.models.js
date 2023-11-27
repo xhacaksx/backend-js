c8f344432123f54f2a0a587f5a7c78ae13df14af
xhacaksx <smartakshat007@gmail.com>
2023-11-27 21:37:18 +0530

    add data models

---
+++ src/models/video.models.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const videoSchema = new mongoose.Schema({
+    videoFile:{
+        type:String, //url
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:Number, //url se hi milega
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+    
+},{timestamps:true});
+
+export const Video = mongoose.model("Video",videoSchema);
