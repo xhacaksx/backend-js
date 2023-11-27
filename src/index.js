dbbb8c558c36b6b0775ca3805a3f88ddff7ba1e8
xhacaksx <smartakshat007@gmail.com>
2023-11-27 16:52:07 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+//require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()(
+  //IIFE
+  async () => {
+    try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+      app.on("error", (error) => {
+        console.log("ERROR: ", error);
+        throw err;
+      });
+
+      app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+      })
+    } catch (error) {
+      console.log("ERROR: ", error);
+      throw err;
+    }
+  }
+)();
+*/
