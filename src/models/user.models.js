889b7f75b580c6e6c5921fc9e00076fb56459f5c
xhacaksx <smartakshat007@gmail.com>
2023-11-29 17:13:51 +0530

    add register controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -56,7 +56,7 @@ userSchema.pre("save", async function (next) {
   //this checks if password is modified or not else
   //everytime we change any other fields it will again hash the password
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
